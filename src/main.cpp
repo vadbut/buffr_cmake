8c513c482b34957c85a63fe55765417f3e48c1c1
vadbut <vadbut@gmail.com>
2021-06-05 17:13:12 -0500

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// buffr_cmake.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
